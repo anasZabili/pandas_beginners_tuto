cf9ec1e46e9cb8094b4acd62ae3d81cbd36f08d8
anas zabili <anaszab78@gmail.com>
2021-07-12 02:04:58 +0200

    final commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import re
 
 # Loading data from file
 pokeData = pd.read_csv('pokemon_data.csv')
@@ -82,3 +83,48 @@ pokeData.to_csv('modifiedPokemon.csv', index=False) # pass the index = False to 
 
 # Saving to txt
 pokeData.to_csv('modifiedPokemon.txt', index=False, sep='\t')
+
+
+# Filtering data (in pandas and numpy and = &, or = |, not = ~)
+# print(pokeData.loc[(pokeData['Type 1'] == 'Grass') | (pokeData['Type 2'] == 'Poison')])
+new_df = pokeData.loc[(pokeData['Type 1'] == 'Grass') & (pokeData['Type 2'] == 'Poison') & (pokeData['HP'] > 70)]
+# reset the index so the new data don't have the old none filtered index
+new_df = new_df.reset_index(drop=True) # by default the old index gonna be stored 
+# in a column to avoid that we can specify drop=True 
+new_df.to_csv('filtered.csv', index=False) 
+print(new_df)
+
+
+#Filter by name following a pattern
+print(pokeData.loc[~pokeData['Name'].str.contains('Mega')])
+
+
+# Filter by regex
+print(pokeData.loc[pokeData['Type 1'].str.contains('fire|grass', flags=re.IGNORECASE, regex = True)])
+print(pokeData.loc[pokeData['Name'].str.contains('^pi[a-z]*', flags=re.IGNORECASE, regex = True)])
+
+
+# Conditional Changes
+pokeData.loc[pokeData['Type 1'] == 'Fire', 'Type 1'] = 'Flamer' 
+# print(pokeData.loc[pokeData['Type 1'].str.contains('Flamer')])
+pokeData.loc[pokeData['Total'] > 500, ['Generation', 'Legendary']] = ['infinite', True]
+# Here we do a multiple change is the total is > 500, Generation and Legendary equal Strong
+print(pokeData)
+# reset the data
+pokeData = pd.read_csv("modifiedPokemon.csv")
+
+
+# Group BY + min + sum + count
+print(pokeData.groupby(['Type 1']).mean().sort_values('HP', ascending=False).iloc[:, :5])
+print(pokeData.groupby(['Type 1']).sum().sort_values('HP', ascending=False).iloc[:, :5])
+# To do a clean count a good practice is to set a count column equal to 1 before de the groupby
+pokeData['count'] = 1
+print(pokeData.groupby(['Type 1', 'Type 2']).count()['count'])
+
+
+# Large data set Chunks
+for df in pd.read_csv("modifiedPokemon.csv", chunksize=5):
+  print("chunk")
+  print(df) # this df only contain 5 rows at each iteration
+
+# Chunk size is the number of line loded at the time in the memorie
